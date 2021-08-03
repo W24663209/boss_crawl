e8de0d7777f83f3c113fa070d23251d26c762407
weizongtang <24663209@qq.com>
2021-08-03 13:09:34 +0800

    实现正常下载

--- src/content/App.vue
+++ src/content/App.vue
@@ -2,13 +2,15 @@
   <div id="app">
     <div class="crx-content">
       <div id="down">
-        <el-button type="success">测试</el-button>
+        <el-button type="success" @click="download">下载</el-button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import {saveAs} from 'file-saver';
+
 export default {
   name: "App.vue",
   data() {
@@ -16,11 +18,21 @@ export default {
   },
   mounted() {
     console.log('插件已注入')
-    this.getTitleInfo()
+    if (localStorage.getItem('crawlDownlaod')) {
+      this.getTitleInfo()
+    }
   },
   methods: {
+    download() {
+      localStorage.setItem('crawlDownlaod', true);
+      window.location.reload()
+    },
     getTitleInfo() {
+      if (!localStorage.getItem('crawlText')) {
+        localStorage.setItem('crawlText', '职位, 城市, 公司, 薪资, 要求, 详情\n')
+      }
       let jobTitleInfo = $('.job-list ul li .job-primary');
+      let text = ''
       jobTitleInfo.each(function () {
         let jobTitle = $(this).find('.job-title');
         let jobName = jobTitle.find('.job-name a').attr('title');
@@ -29,8 +41,21 @@ export default {
         let info = $(this).find('.company-text p').text();
         let price = $(this).find('.job-limit .red').text();
         let requirements = $(this).find('.job-limit p').text();
+        text += jobName + ',' + jobArea + ',' + name + ',' + price + ',' + requirements + ',' + info + '\n'
         console.log(jobName, jobArea, name, price, requirements, info)
       })
+      let item = localStorage.getItem('crawlText');
+      item += text
+      localStorage.setItem('crawlText', item)
+      if (!$('.page .next').hasClass('disabled')) {
+        window.location.href = $('.page .next').attr('href')
+      } else {
+        var blob = new Blob([item], {type: "text/plain;charset=utf-8"});
+        saveAs(blob, $('.ipt-wrap input').val() + ".csv");
+        localStorage.setItem('crawlText', undefined);
+        console.log('下载完成')
+        localStorage.setItem('crawlDownlaod', undefined)
+      }
     }
   }
 }
